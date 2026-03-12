010bf3839b460164d40191b2bb55aab6bf3b88e6
Дмитрий <tdima822@gmail.com>
2026-03-12 17:50:31 +0700

    style: add missing semicolons

--- src/games/even.js
+++ src/games/even.js
@@ -1,23 +1,23 @@
-import runGame from '../index.js'
+import runGame from '../index.js';
 
 // Функция для проверки чётности числа
-const isEven = number => number % 2 === 0
+const isEven = number => number % 2 === 0;
 
 // Функция для получения случайного числа от 1 до 100
-const getRandomNumber = () => Math.floor(Math.random() * 100) + 1
+const getRandomNumber = () => Math.floor(Math.random() * 100) + 1;
 
 // Правило игры
-const gameRule = 'Answer "yes" if the number is even, otherwise answer "no".'
+const gameRule = 'Answer "yes" if the number is even, otherwise answer "no".';
 
 // Генерация одного раунда
 const generateRound = () => {
-  const question = getRandomNumber()
-  const correctAnswer = isEven(question) ? 'yes' : 'no'
+  const question = getRandomNumber();
+  const correctAnswer = isEven(question) ? 'yes' : 'no';
 
-  return { question, correctAnswer }
-}
+  return { question, correctAnswer };
+};
 
 // Запуск игры
-const playGame = () => runGame(gameRule, generateRound)
+const playGame = () => runGame(gameRule, generateRound);
 
-export default playGame
+export default playGame;
